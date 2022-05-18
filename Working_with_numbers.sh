949e7a30527084449f3542031b11afb14a2b3224
Louis <lfosu3@gmail.com>
2022-05-18 16:31:26 +0000

    Assignment for beginner class from Eng team

---
+++ Working_with_numbers.sh
@@ -0,0 +1,12 @@
+clear
+echo "WORKING WITH NUMBERS IN SHELL SCRIPT"
+X=60
+Y=12
+sum=$(($X + $Y))
+difference=$(($X - $Y))
+product=$(($X * $Y))
+quotient=$(($X / $Y))
+echo "$sum"
+echo "$difference"
+echo "$product"
+echo "$quotient"
